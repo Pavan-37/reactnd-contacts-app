25fb9bb908e2298760293af1dcabde7a4fbe1b45
Pavan-37 <pvnkmrsingh003@gmail.com>
2018-09-07 12:55:47 +0530

    Render default contacts

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
-import React, { Component } from 'react'
-import ListContacts from './ListContacts'
-import * as ContactsAPI from './utils/ContactsAPI'
-import CreateContact from './CreateContact'
-import { Route } from 'react-router-dom'
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import ListContacts from "./ListContacts";
 
-class App extends Component {
-  state = {
-    contacts: []
-  }
-  componentDidMount() {
-    ContactsAPI.getAll()
-      .then((contacts) => {
-        this.setState(() => ({
-          contacts
-        }))
-      })
-  }
-  removeContact = (contact) => {
-    this.setState((currentState) => ({
-      contacts: currentState.contacts.filter((c) => {
-        return c.id !== contact.id
-      })
-    }))
+const contacts = [
+    {
+      "id": "karen",
+      "name": "Karen Isgrigg",
+      "handle": "karen_isgrigg",
+      "avatarURL": "http://localhost:5001/karen.jpg"
+    },
+    {
+      "id": "richard",
+      "name": "Richard Kalehoff",
+      "handle": "richardkalehoff",
+      "avatarURL": "http://localhost:5001/richard.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "handle": "tylermcginnis",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+   ];
 
-    ContactsAPI.remove(contact)
-  }
-  createContact = (contact) => {
-    ContactsAPI.create(contact)
-      .then((contact) => {
-        this.setState((currentState) => ({
-          contacts: currentState.contacts.concat([contact])
-        }))
-      })
-  }
-  render() {
-    return (
-      <div>
-        <Route exact path='/' render={() => (
-          <ListContacts
-            contacts={this.state.contacts}
-            onDeleteContact={this.removeContact}
-          />
-        )} />
-        <Route path='/create' render={({ history }) => (
-          <CreateContact
-            onCreateContact={(contact) => {
-              this.createContact(contact)
-              history.push('/')
-            }}
-          />
-        )} />
-      </div>
-    )
-  }
+class App extends Component{
+    render(){
+        return(
+            <ListContacts contacts={contacts} />
+        );
+    }
 }
 
-export default App
+ReactDOM.render(<App />, document.getElementById("root"));
+
+export default App;
